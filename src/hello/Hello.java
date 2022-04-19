421ace215bd397e3c5ade415af23d2901c818a34
Sumeyra Tastan <sumeyratastan.st@gmail.com>
2022-04-19 11:31:19 +0100

    doing final work on Hello, feature

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
     public static void main(String[] args) {
 
         System.out.println("Hello world");
+        System.out.println(" EU8 learning Git and GitHub.sout");
 
 
     }
